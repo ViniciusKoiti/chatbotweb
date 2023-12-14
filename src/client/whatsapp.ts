42ff4ecbfa05aba615f0d219a8428cff8f41f442
Vinicius Koiti <viniciusnakahara@gmail.com>
2023-12-14 20:58:03 -0300

    Alterações recebendo mensagem

--- src/client/whatsapp.ts
+++ src/client/whatsapp.ts
@@ -1,10 +1,16 @@
 import { Client } from 'whatsapp-web.js';
+import qrcode from 'qrcode-terminal';
 
 const client = new Client({
     puppeteer: {
 		args: ['--no-sandbox'],
 	}
 });
+
+client.on('message', message => {
+	console.log(message.body);
+});
+ 
 
 client.on('qr', (qr: string) => {
     console.log('QR RECEIVED', qr);
